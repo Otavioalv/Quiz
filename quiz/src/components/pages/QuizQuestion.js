8dbebd72c5f9149ca954b77d4b9c93a2c4c64500
Otávio Gabriel <107057360+Otavioalv@users.noreply.github.com>
2023-04-14 15:01:36 -0400

    updates

--- quiz/src/components/pages/QuizQuestion.js
+++ quiz/src/components/pages/QuizQuestion.js
@@ -1,8 +1,19 @@
 import styles from './QuizQuestion.module.css'
 
+import questions from '../script/quizes.json'
+
 function QuizQuestion() {
     return ( 
-        <div>Quatoes do quiaz </div>
+        <div>
+            <h1>{questions[1].question}</h1>
+
+            <div>
+                <ol>
+                    
+                    <li>{questions[1].options[0]}</li>
+                </ol>
+            </div>
+        </div>
     )
 }
 
--- quiz/src/components/script/teste.js
+++ quiz/src/components/script/teste.js
@@ -1,48 +1,15 @@
-var str = "a) char b) float c) int d) double";
-var res = [];
-var a = 0;
+var str = "a) ! b) && c) || d) > ";
 
-/* for(var i = 0; i < str.length; i++) { 
-    if(str[i] != ' '){
-        res.push(str[i])
-        console.log(res)
+for(var i = 0; i < str.length; i++) {   
+    if(str[i] === ')') {
+        str = str.slice(0, i-1) + "," + str.slice(i);
     }
-} */
-
-str = str.replaceAll(')', '')
-console.log(str);
-
-
-for(var i = 0; i < str.length; i++) { 
-    
-    if(i == 0) {
-        console.log(str[i]) 
-        str = str.replace(str[i], '');
-
-        console.log(str) 
+    else if(str[i].match(/[a-zA-Z]/) && str[i-1] == ')') {
+        str = str.slice(0, i) + " " + str.slice(i+1); 
     }
-    else if (str[i] == ' ') {
-        console.log(str[i+1], str[i+2]) 
-        str = str.replace(str[i], '');
-
-        console.log(str) 
-    }
-    
-    if(a == 1)
-        a = 0;
-    else    
-        a = 1;
 }
 
+str = str.replaceAll(")", "")
+str = str.replaceAll(" ", '"')
 
-/* res = res.join('');
-console.log(res)
-
-for(var i = 0; i < res.length; i++) {
-    if(res[i] == ')' ){
-        console.log(res[i-1]);
-
-        //b = b.slice(0, a - 1) + b.slice(a + 1);
-    }
-} */
-
+console.log(str); 
