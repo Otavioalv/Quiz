90974da479c136c12ac6d5076eca2844c6fdfbdc
Otávio Gabriel <107057360+Otavioalv@users.noreply.github.com>
2023-05-01 13:27:24 -0400

    update

--- quiz/src/components/pages/QuizQuestion.js
+++ quiz/src/components/pages/QuizQuestion.js
@@ -1,18 +1,47 @@
 import styles from './QuizQuestion.module.css'
 
 import questions from '../script/quizes.json'
+import { useState } from 'react'
 
 function QuizQuestion() {
-    return ( 
-        <div>
-            <h1>{questions[0].question}</h1>
-            <div>
-                <ol>
-                    <li>{questions[0].options[0]}</li>
-                </ol>
+    
+    const [pos, setPos] = useState(0)
+    const [pts, setPts] = useState(0)
+
+    function optionChose(option) {
+        if(option === questions[pos].response){
+            console.log("Acertou")
+            setPts(pts+1)
+        }
+        else    
+            console.log("errou")
+
+        setPos(pos+1)
+
+        if(pos === 9)
+            setPos(0)
+    }
+    
+    return (
+        <section className={styles.homeContainer}>
+            
+            <div className={styles.homeContainer__card}>
+                <h1>{questions[pos].question}<span>{pts}</span></h1>
             </div>
-        </div>
-    )
+
+                <div className={styles.homeContainer__list}>
+                    <ul>
+                        {questions[pos].options.map((option, key) => (
+                            <li key={key}>
+                                <button onClick={() => optionChose(option)} className={styles.homeContainer__btn}>
+                                    {option}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+        </section>
+  );
 }
 
-export default QuizQuestion
+export default QuizQuestion;
