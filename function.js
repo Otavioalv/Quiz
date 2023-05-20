2fbbbff5c2cd05c7db24f68652f7736070847db8
Otávio Gabriel <107057360+Otavioalv@users.noreply.github.com>
2023-05-20 09:50:11 -0400

    UPDATES!!!!

--- function.js
+++ function.js
@@ -2,20 +2,49 @@
 /* inicio Função do quiz.html */
 
 function startQuiz(){
-    console.log("iniciado")
+    console.log("iniciado");
 }
 
 /* Fim função quiz.html */
 
 
 /* inicion Funnção quizQuestions.html*/
+var pos = 0
+var question = document.getElementById("question");
+ /*    opc_0 = document.getElementById("opc-0"),
+    opc_1 = document.getElementById("opc-1"),
+    opc_2= document.getElementById("opc-2"),
+    opc_3 = document.getElementById("opc-3"); */
 
-function optionChose(opc) {
+
+var opcs = document.querySelectorAll("#opcs li button");
+console.log(opcs);
+
+function optionChose(opc) { // opc, e a posição da opções de quests[].options[opc]
+
+
     fetch('quests.json')
     .then(response => response.json())
     .then(quests => {
-        console.log(quests[0].options[opc])
-    })
-}
+        const option = quests[pos].options[opc]
+        
+        //console.log(option)
+        if(option === quests[pos].response) {
+            console.log("\nopção correta\n");
+        } 
 
+        console.log(pos);
+        pos++;
+        console.log(pos);
+
+        question.innerHTML = quests[pos].question;
+
+        Array.from(opcs).map(function(opc, p) {
+            console.log(`Nome: ${opc.innerHTML}  posição: ${p}`)
+        
+            opc.innerHTML = quests[pos].options[p]
+        });  
+        
+    });
+}
 /* fim função quizQuestions.html */
